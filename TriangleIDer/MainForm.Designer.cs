8c085a72460047883668a9a86b6e4bace976bcbd
Ryan Hintze <ryanshintze@gmail.com>
2018-03-30 16:08:34 -0600

    Triangle Identifier!!!

---
+++ TriangleIDer/MainForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TriangleIDer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ParameterPanel = new System.Windows.Forms.Panel();
+            this.TriangleViewPanel = new System.Windows.Forms.Panel();
+            this.TB_SideC = new System.Windows.Forms.TextBox();
+            this.TB_SideB = new System.Windows.Forms.TextBox();
+            this.TB_SideA = new System.Windows.Forms.TextBox();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ParameterPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ParameterPanel
+            // 
+            this.ParameterPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ParameterPanel.Controls.Add(this.TriangleViewPanel);
+            this.ParameterPanel.Controls.Add(this.TB_SideC);
+            this.ParameterPanel.Controls.Add(this.TB_SideB);
+            this.ParameterPanel.Controls.Add(this.TB_SideA);
+            this.ParameterPanel.Controls.Add(this.StatusLabel);
+            this.ParameterPanel.Controls.Add(this.label3);
+            this.ParameterPanel.Controls.Add(this.label2);
+            this.ParameterPanel.Controls.Add(this.label1);
+            this.ParameterPanel.Location = new System.Drawing.Point(13, 13);
+            this.ParameterPanel.Name = "ParameterPanel";
+            this.ParameterPanel.Size = new System.Drawing.Size(268, 133);
+            this.ParameterPanel.TabIndex = 0;
+            // 
+            // TriangleViewPanel
+            // 
+            this.TriangleViewPanel.Location = new System.Drawing.Point(161, 3);
+            this.TriangleViewPanel.Name = "TriangleViewPanel";
+            this.TriangleViewPanel.Size = new System.Drawing.Size(104, 72);
+            this.TriangleViewPanel.TabIndex = 13;
+            this.TriangleViewPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.TriangleViewPanel_Paint);
+            // 
+            // TB_SideC
+            // 
+            this.TB_SideC.Location = new System.Drawing.Point(55, 55);
+            this.TB_SideC.Name = "TB_SideC";
+            this.TB_SideC.Size = new System.Drawing.Size(100, 20);
+            this.TB_SideC.TabIndex = 12;
+            this.TB_SideC.TextChanged += new System.EventHandler(this.TB_Side_TextChanged);
+            this.TB_SideC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.sideTextBox_KeyPress);
+            // 
+            // TB_SideB
+            // 
+            this.TB_SideB.Location = new System.Drawing.Point(55, 29);
+            this.TB_SideB.Name = "TB_SideB";
+            this.TB_SideB.Size = new System.Drawing.Size(100, 20);
+            this.TB_SideB.TabIndex = 11;
+            this.TB_SideB.TextChanged += new System.EventHandler(this.TB_Side_TextChanged);
+            this.TB_SideB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.sideTextBox_KeyPress);
+            // 
+            // TB_SideA
+            // 
+            this.TB_SideA.Location = new System.Drawing.Point(55, 3);
+            this.TB_SideA.Name = "TB_SideA";
+            this.TB_SideA.Size = new System.Drawing.Size(100, 20);
+            this.TB_SideA.TabIndex = 10;
+            this.TB_SideA.TextChanged += new System.EventHandler(this.TB_Side_TextChanged);
+            this.TB_SideA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.sideTextBox_KeyPress);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StatusLabel.Location = new System.Drawing.Point(3, 99);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Padding = new System.Windows.Forms.Padding(1);
+            this.StatusLabel.Size = new System.Drawing.Size(270, 15);
+            this.StatusLabel.TabIndex = 9;
+            this.StatusLabel.Text = "Please Input Numeric Values > 0 Into Sides A, B, and C";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 54);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(4);
+            this.label3.Size = new System.Drawing.Size(46, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Side C";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 28);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(4);
+            this.label2.Size = new System.Drawing.Size(46, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Side B";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 2);
+            this.label1.Margin = new System.Windows.Forms.Padding(2);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(4);
+            this.label1.Size = new System.Drawing.Size(46, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Side A";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 157);
+            this.Controls.Add(this.ParameterPanel);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(309, 196);
+            this.MinimumSize = new System.Drawing.Size(309, 196);
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Triangle Identifier";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ParameterPanel.ResumeLayout(false);
+            this.ParameterPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Panel ParameterPanel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label StatusLabel;
+        private TextBox TB_SideA;
+        private TextBox TB_SideC;
+        private TextBox TB_SideB;
+        private Panel TriangleViewPanel;
+    }
+}
+
---
+++ TriangleIDer/MainForm.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TriangleIDer
+{
+    public partial class MainForm : Form
+    {
+        private const double _EPSILON = .00000001;
+        double a = 0;
+        double b = 0;
+        double c = 0;
+
+        private bool validTriangleAvailable = false;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public static double EPSILON => _EPSILON;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void sideTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox textbox = ((sender) as TextBox);
+
+            //Don't Allow any characters except Control Characters, Digits, and '.'
+            if(!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            //Only allow one '.'
+            if((e.KeyChar =='.') && textbox.Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+            }
+            
+            //TODO Handle Paste
+        }
+
+        private void TB_Side_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textbox = sender as TextBox;
+            double value;
+            StatusLabel.Text = "Please Input Numeric Values > 0 Into Sides A, B, and C";
+            validTriangleAvailable = false;
+            TriangleViewPanel.Invalidate();
+            //Validate and if successful, change value
+            if (!ValidateNumber(textbox, out value))
+                return;
+
+            //Assign value to corresponding triangle
+            if (sender == TB_SideA)
+                a = value;
+            if (sender == TB_SideB)
+                b = value;
+            if (sender == TB_SideC)
+                c = value;
+
+            //Check That they are all Non-Zero
+            if (!(a * b * c > 0))
+                return;
+
+            //Possible Triangle... Start Checks
+            //sort into an array for less checks
+            double[] sides = new double[3] { a, b, c };
+            Array.Sort(sides);
+            //SQRD array for multiple checks
+            double[] sides_sqrd = new double[3] { sides[0] * sides[0], sides[1] * sides[1], sides[2] * sides[2] };
+
+            //Check invalid triangle
+            if (sides[0] + sides[1] <= sides[2])
+            {
+                StatusLabel.Text = "Those lengths do not form a Triangle.";
+                return;
+            }
+            validTriangleAvailable = true;
+            TriangleViewPanel.Invalidate();
+            StatusLabel.Text = "Those lengths form a Triangle.";
+            if (RightTriangleCheck_PreSQRD(sides_sqrd))
+                StatusLabel.Text = "Those lengths form a Right Triangle";
+            if(sides[0] == sides[1] || sides[1]== sides[2] || sides[0]==sides[2])
+            {
+                if (sides[0] == sides[1] && sides[1] == sides[2])
+                    StatusLabel.Text = "Those lengths form an Equilateral Triangle";
+                else if (RightTriangleCheck_PreSQRD(sides_sqrd))
+                    StatusLabel.Text = "Those lengths form a Right Isosceles Triangle";
+                else
+                    StatusLabel.Text = "Those lengths form an Isosceles Triangle.";
+
+            }
+
+        }
+
+        private bool RightTriangleCheck_PreSQRD(double[] sides_sqrd)
+        {
+            return Math.Abs(sides_sqrd[0] + sides_sqrd[1] - sides_sqrd[2]) < EPSILON;
+        }
+
+        private bool ValidateNumber(TextBox textbox, out double value)
+        {
+            value = 0;
+            string text = textbox.Text;
+            if (text.Length == 0)
+                return false;
+            bool periodSet = false;
+
+            //Check each character for valid input
+            foreach (char c in text)
+            {
+                if (!(char.IsNumber(c) || (c == '.' && !periodSet)))
+                    //TODO Show ToolTip
+                    return false;
+                if (c == '.')
+                    periodSet = true;
+            }
+
+            value = Double.Parse(text);
+            return true;
+        }
+
+        private void TriangleViewPanel_Paint(object sender, PaintEventArgs e)
+        {
+            if (!validTriangleAvailable)
+                return;
+
+            double width = TriangleViewPanel.Width-2;
+            double height = TriangleViewPanel.Height-2;
+
+            double Ax = 0;
+            double Ay = 0;
+            
+            double Bx = a;
+            double By = 0;
+
+            //Calculate Cx Cy using law of cosines 
+            //C_theta = arccos((c^2 - a^2 - b^2)/(2ab)
+            double C_theta = Math.Acos((a * a + c * c - b * b) / (2 * a * c));
+            double Cx = Math.Cos(C_theta) * c;
+            double Cy = Math.Sin(C_theta) * c;
+
+            //Scale Triangle to fit inside the Panel
+            double MaxX = a;
+            if (Cx > MaxX)
+                MaxX = Cx;
+            if(Cx < 0)
+            {
+                MaxX = a - Cx;
+
+                //Shift right
+                Ax -= Cx;
+                Bx -= Cx;
+                Cx = 0;
+            }
+            //Compare porportions
+            double ScaleFactor = width / MaxX;
+            double yScaleFactor = height / Cy;
+            if(MaxX < width || Cy < height)
+            {
+                //Scale Down
+                ScaleFactor = (ScaleFactor < yScaleFactor) ? ScaleFactor : yScaleFactor;
+            } else
+            {
+                //Scale Up
+                ScaleFactor = (ScaleFactor > yScaleFactor) ? ScaleFactor : yScaleFactor;
+            }
+
+            Ax *= ScaleFactor;
+            Bx *= ScaleFactor;
+            Cx *= ScaleFactor;
+            Cy *= ScaleFactor;
+
+            //Center
+            Ay = (height - Cy) / 2.0;
+            By += Ay;
+            Cy += Ay;
+
+            MaxX *= ScaleFactor;
+            double xShift = (width - MaxX) / 2.0;
+
+            Ax += xShift;
+            Bx += xShift;
+            Cx += xShift;
+
+
+            Point ptA = new Point((int)Ax+1,1);
+            Point ptB = new Point((int)Bx+1, 1);
+            Point ptC = new Point((int)Cx+1, (int)Cy);
+
+            //Draw
+            Pen p = new Pen(Color.CornflowerBlue, 2);
+            e.Graphics.DrawLine(p, ptA, ptB);
+            e.Graphics.DrawLine(p, ptB, ptC);
+            e.Graphics.DrawLine(p, ptA, ptC);
+        }
+
+        private bool IsValidTriangle(double[] sortedSides)
+        {
+            //Check invalid triangle
+            if (sortedSides[0] + sortedSides[1] <= sortedSides[2])
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
